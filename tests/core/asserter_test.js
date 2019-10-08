f4ef90d469456103d8b4e2a3dc4dbe6a4b7647d6
Nahuel Garbezza <n.garbezza@gmail.com>
2019-10-07 23:41:31 -0300

    :sparkles: allow to pass regex objects to the raise() matcher (fixes #39)

--- tests/core/asserter_test.js
+++ tests/core/asserter_test.js
@@ -2,6 +2,7 @@
 
 const { suite, test, assert } = require('../../testy');
 const { Asserter } = require('../../lib/asserter');
+const { TestSucceeded } = require('../../lib/test_result');
 const I18n = require('../../lib/i18n');
 
 const fakeRunner = {
@@ -13,12 +14,12 @@ const fakeRunner = {
 const asserter = new Asserter(fakeRunner);
 
 function expectFailDueTo(failureMessage) {
-  assert.isFalse(fakeRunner.result().isSuccess());
+  assert.isTrue(fakeRunner.result().isFailure());
   assert.areEqual(fakeRunner.result().failureMessage(), failureMessage);
 }
 
 function expectSuccess() {
-  assert.isTrue(fakeRunner.result().isSuccess());
+  assert.areEqual(fakeRunner.result(), new TestSucceeded());
 }
 
 suite('assertions behavior', () => {
@@ -222,5 +223,31 @@ suite('assertions behavior', () => {
     asserter.isNotEmpty(['hey']);
     
     expectSuccess();
+  });
+  
+  // Exception assertions
+  
+  test('raises() can receive a string and it passes when the exact string is expected', () => {
+    asserter.that(() => { throw 'an error happened' }).raises('an error happened');
+    
+    expectSuccess();
+  });
+  
+  test('raises() can receive a regex and it passes when it matches the thrown string', () => {
+    asserter.that(() => { throw 'an error happened' }).raises(/error/);
+    
+    expectSuccess();
+  });
+  
+  test('raises() can receive a regex and it passes when it matches the thrown error with message', () => {
+    asserter.that(() => { throw new TypeError('things happened') }).raises(/happened/);
+    
+    expectSuccess();
+  });
+  
+  test('raises() can receive a regex and it fails if there is not a match in the error message', () => {
+    asserter.that(() => { throw 'a terrible error' }).raises(/happiness/);
+    
+    expectFailDueTo('Expected error /happiness/ to happen, but got \'a terrible error\' instead');
   });
 });
