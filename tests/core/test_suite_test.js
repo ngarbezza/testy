81ef208b415033b5556fca31ae7016c87249e40f
Nahuel Garbezza <n.garbezza@gmail.com>
2020-08-19 21:20:47 -0300

    :white_check_mark: add a test to assert error behavior when tests have no assertions; add a tests factory to help creating objects for testing

--- tests/core/test_suite_test.js
+++ tests/core/test_suite_test.js
@@ -2,37 +2,30 @@
 
 const { suite, test, before, assert } = require('../../testy');
 const TestSuite = require('../../lib/test_suite');
-const Test = require('../../lib/test');
 const { Asserter } = require('../../lib/asserter');
 const TestRunner = require('../../lib/test_runner');
+const { aPassingTest, aFailingTest, anErroredTest, aPendingTest } = require('../support/tests_factory');
 
 const noop = () => {};
 const emptyRunnerCallbacks = { onFinish: noop };
 const emptySuiteCallbacks = { onStart: noop, onFinish: noop };
-const emptyTestCallbacks = {
-  whenErrored: noop,
-  whenFailed: noop,
-  whenPending: noop,
-  whenSkipped: noop,
-  whenSuccess: noop,
-};
 
 const newEmptySuite = () => suiteNamed('myTestSuite');
 const suiteNamed = suiteName => new TestSuite(suiteName, () => {}, emptySuiteCallbacks);
 
 suite('test suite behavior', () => {
   let runner, asserter, mySuite;
-  let passingTest, failedTest, erroredTest, pendingTest;
+  let passingTest, failingTest, erroredTest, pendingTest;
   
   before(() => {
     runner = new TestRunner(emptyRunnerCallbacks);
     asserter = new Asserter(runner);
     mySuite = newEmptySuite();
     runner.addSuite(mySuite);
-    passingTest = new Test('a pure success', () => asserter.isTrue(true), emptyTestCallbacks);
-    failedTest = new Test('a true failure', () => asserter.isFalse(true), emptyTestCallbacks);
-    erroredTest = new Test('an unexpected error', () => { throw 'oops'; }, emptyTestCallbacks);
-    pendingTest = new Test('a work in progress', undefined, emptyTestCallbacks);
+    passingTest = aPassingTest(asserter);
+    failingTest = aFailingTest(asserter);
+    erroredTest = anErroredTest();
+    pendingTest = aPendingTest();
   });
   
   test('more than one before block is not allowed', () => {
@@ -45,11 +38,11 @@ suite('test suite behavior', () => {
   
   test('reporting failures and errors', () => {
     mySuite.addTest(passingTest);
-    mySuite.addTest(failedTest);
+    mySuite.addTest(failingTest);
     mySuite.addTest(erroredTest);
     runner.run();
 
-    assert.that(mySuite.allFailuresAndErrors()).includesExactly(failedTest, erroredTest);
+    assert.that(mySuite.allFailuresAndErrors()).includesExactly(failingTest, erroredTest);
   });
   
   test('an empty suite can be executed and it reports zero tests', () => {
