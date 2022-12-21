e4fc7e0194c4c883959001d3e4379fef15553c97
Nahuel Garbezza <n.garbezza@gmail.com>
2022-12-21 02:50:58 -0300

    :white_check_mark: add tests for console ui

--- tests/ui/console_ui_test.js
+++ tests/ui/console_ui_test.js
@@ -5,6 +5,7 @@ const { suite, test, before, assert } = require('../../lib/testy');
 const ConsoleUI = require('../../lib/console_ui');
 const FakeConsole = require('./fake_console');
 const FakeProcess = require('./fake_process');
+const Configuration = require('../../lib/configuration');
 
 suite('Console UI', () => {
   let fakeProcess, fakeConsole;
@@ -20,5 +21,26 @@ suite('Console UI', () => {
     assert.that(fakeProcess.lastExitCode()).isEqualTo(ConsoleUI.failedExitCode());
     assert.that(fakeConsole.messages().length).isEqualTo(1);
     assert.that(fakeConsole.messages()[0]).matches(/a message/);
+  });
+
+  test('successful exit code is zero', () => {
+    assert.areEqual(ConsoleUI.successfulExitCode(), 0);
+  });
+
+  test('failed exit code is one', () => {
+    assert.areEqual(ConsoleUI.failedExitCode(), 1);
+  });
+
+  test('prints start and end messages', async () => {
+    const console = new ConsoleUI(fakeProcess, fakeConsole);
+    const config = Configuration.current();
+    await console.start(config, [], () => {});
+    assert.that(fakeConsole.messages().length).isEqualTo(8);
+    assert.that(fakeConsole.messages()[0]).matches(/timer started/);
+    assert.that(fakeConsole.messages()[2]).matches(/Starting Testy!/);
+    assert.that(fakeConsole.messages()[3]).matches(/Running tests in/);
+    assert.that(fakeConsole.messages()[4]).matches(/Fail fast/);
+    assert.that(fakeConsole.messages()[5]).matches(/Random order/);
+    assert.that(fakeConsole.messages()[7]).matches(/timer ended/);
   });
 });
