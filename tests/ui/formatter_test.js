f2d42a06578603a1fab3c16c2f43743eda05cef9
Nahuel Garbezza <n.garbezza@gmail.com>
2023-01-31 05:13:45 -0300

    :children_crossing: make timeout error message internationalized

--- tests/ui/formatter_test.js
+++ tests/ui/formatter_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { assert, before, suite, test } from '../../lib/testy.js';
-import { runSingleTest, withRunner } from '../support/runner_helpers.js';
+import { resultOfASuiteWith, withRunner } from '../support/runner_helpers.js';
 import { aPendingTest, aTestWithBody } from '../support/tests_factory.js';
 
 import { Formatter } from '../../lib/ui/formatter.js';
@@ -28,7 +28,7 @@ suite('formatter', () => {
   test('display pending status in yellow including reason', async() => {
     await withRunner(async(runner, _assert, _fail, pending) => {
       const pendingTest = aTestWithBody(() => pending.dueTo('in a rush'));
-      await runSingleTest(runner, pendingTest);
+      await resultOfASuiteWith(runner, pendingTest);
       formatter.displayPendingResult(pendingTest);
       const testResultMessage = '[\u001b[33m\u001b[1mWIP\u001b[0m] \u001b[33mjust a test\u001b[0m';
       const pendingReasonMessage = '  => in a rush';
@@ -39,7 +39,7 @@ suite('formatter', () => {
   test('display error status in red when not specifying a reason', async() => {
     await withRunner(async(runner, _assert, _fail, pending) => {
       const pendingTest = aTestWithBody(() => pending.dueTo());
-      await runSingleTest(runner, pendingTest);
+      await resultOfASuiteWith(runner, pendingTest);
       formatter.displayFailureResult(pendingTest, 'error');
       const testResultMessage = '[\u001b[31m\u001b[1mERROR\u001b[0m] \u001b[31mjust a test\u001b[0m';
       const pendingReasonMessage = '  => In order to mark a test as pending, you need to specify a reason.';
@@ -50,7 +50,7 @@ suite('formatter', () => {
   test('display failure status in red including reason', async() => {
     await withRunner(async(runner, _assert, fail) => {
       const failedTest = aTestWithBody(() => fail.with('I wanted to fail'));
-      await runSingleTest(runner, failedTest);
+      await resultOfASuiteWith(runner, failedTest);
       formatter.displayFailureResult(failedTest, 'fail');
       const testResultMessage = '[\u001b[31m\u001b[1mFAIL\u001b[0m] \u001b[31mjust a test\u001b[0m';
       const failureDetailMessage = '  => I wanted to fail';
@@ -61,7 +61,7 @@ suite('formatter', () => {
   test('display failure status in red including default explicit failure reason', async() => {
     await withRunner(async(runner, _assert, fail) => {
       const failedTest = aTestWithBody(() => fail.with());
-      await runSingleTest(runner, failedTest);
+      await resultOfASuiteWith(runner, failedTest);
       formatter.displayFailureResult(failedTest, 'fail');
       const testResultMessage = '[\u001b[31m\u001b[1mFAIL\u001b[0m] \u001b[31mjust a test\u001b[0m';
       const failureDetailMessage = '  => Explicitly failed';
@@ -72,7 +72,7 @@ suite('formatter', () => {
   test('display pending status in yellow and no reason if the test is empty', async() => {
     await withRunner(async runner => {
       const pendingTest = aPendingTest();
-      await runSingleTest(runner, pendingTest);
+      await resultOfASuiteWith(runner, pendingTest);
       formatter.displayPendingResult(pendingTest);
       const testResultMessage = '[\u001b[33m\u001b[1mWIP\u001b[0m] \u001b[33ma work in progress\u001b[0m';
       assert.that(fakeConsole.messages()).includesExactly(testResultMessage);
