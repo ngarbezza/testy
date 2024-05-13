078cfbd1d9bf6c5cbfd24285163fe3b12a2d9870
mariabelenamat <belen.amat29@gmail.com>
2024-05-12 21:06:34 -0300

    fixing linting

--- tests/core/assertions/numeric_assertions_test.js
+++ tests/core/assertions/numeric_assertions_test.js
@@ -43,75 +43,75 @@ suite('numeric assertions', () => {
     expectSuccess(result);
   });
 
-  test('isGreaterThan passes if the number is strictly smaller', async () => {
+  test('isGreaterThan passes if the number is strictly smaller', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isGreaterThan(1));
 
     expectSuccess(result);
-  })
+  });
 
-  test('isGreaterThan fails if the numbers are equal', async () => {
+  test('isGreaterThan fails if the numbers are equal', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isGreaterThan(2));
 
     expectFailureOn(result, I18nMessage.of('expectation_be_greater_than', '2', '2'));
-  })
+  });
 
-  test('isGreaterThan fails if the number is bigger', async () => {
+  test('isGreaterThan fails if the number is bigger', async() => {
     const result = await resultOfATestWith(assert => assert.that(1).isGreaterThan(2));
 
     expectFailureOn(result, I18nMessage.of('expectation_be_greater_than', '1', '2'));
-  })
+  });
 
-  test('isLessThan passes if the number is strictly bigger', async () => {
+  test('isLessThan passes if the number is strictly bigger', async() => {
     const result = await resultOfATestWith(assert => assert.that(1).isLessThan(2));
 
     expectSuccess(result);
   });
 
-  test('isLessThan fails if the numbers are equal', async () => {
+  test('isLessThan fails if the numbers are equal', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isLessThan(2));
 
     expectFailureOn(result, I18nMessage.of('expectation_be_less_than', '2', '2'));
-  })
+  });
 
-  test('isLessThan fails if the number is smaller', async () => {
+  test('isLessThan fails if the number is smaller', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isLessThan(1));
 
     expectFailureOn(result, I18nMessage.of('expectation_be_less_than', '2', '1'));
-  })
+  });
 
-  test('isGreaterThanOrEqualTo passes if the number is strictly smaller', async () => {
+  test('isGreaterThanOrEqualTo passes if the number is strictly smaller', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isGreaterThanOrEqualTo(1));
 
     expectSuccess(result);
-  })
+  });
 
-  test('isGreaterThanOrEqualTo passes if the numbers are equal', async () => {
+  test('isGreaterThanOrEqualTo passes if the numbers are equal', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isGreaterThanOrEqualTo(2));
 
     expectSuccess(result);
-  })
+  });
 
-  test('isGreaterThanOrEqualTo fails if the number is bigger', async () => {
+  test('isGreaterThanOrEqualTo fails if the number is bigger', async() => {
     const result = await resultOfATestWith(assert => assert.that(1).isGreaterThanOrEqualTo(2));
 
     expectFailureOn(result, I18nMessage.of('expectation_be_greater_than_or_equal', '1', '2'));
-  })
+  });
 
-  test('isLessThanOrEqualTo passes if the number is strictly bigger', async () => {
+  test('isLessThanOrEqualTo passes if the number is strictly bigger', async() => {
     const result = await resultOfATestWith(assert => assert.that(1).isLessThanOrEqualTo(2));
 
     expectSuccess(result);
-  })
+  });
 
-  test('isLessThanOrEqualTo passes if the numbers are equal', async () => {
+  test('isLessThanOrEqualTo passes if the numbers are equal', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isLessThanOrEqualTo(2));
 
     expectSuccess(result);
-  })
+  });
 
-  test('isLessThanOrEqualTo fails if the number is smaller', async () => {
+  test('isLessThanOrEqualTo fails if the number is smaller', async() => {
     const result = await resultOfATestWith(assert => assert.that(2).isLessThanOrEqualTo(1));
 
     expectFailureOn(result, I18nMessage.of('expectation_be_less_than_or_equal', '2', '1'));
-  })
+  });
 });
