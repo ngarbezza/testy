6adbe36b71d4f6b7cef3259a917527ec7871c085
mariabelenamat <belen.amat29@gmail.com>
2025-04-13 12:24:37 -0300

    Moving logic from Asserter and Assertion to helpers file

---
+++ tests/core/assertions/test_result_assertions_test.js
@@ -0,0 +1,25 @@
+import {assert, suite, test} from "../../../lib/testy.js";
+import {TestResult} from "../../../lib/core/test_result.js";
+import {expectFailureOn, expectSuccess} from "../../support/assertion_helpers.js";
+import {I18nMessage} from "../../../lib/i18n/i18n_messages.js";
+
+
+suite('test result assertions', () => {
+    test('expectSuccess passes if the sent test is successful', () => {
+        const testResult = TestResult.success();
+        expectSuccess(testResult);
+    });
+
+    test('expectSuccess fails if sent object is not a test result', () => {
+        assert.that(() => expectSuccess(3)).raises(new Error(I18nMessage.of('invalid_object_in_test_result_assertion', typeof 3)))
+    });
+
+    test('expectSuccess passes if test is failed', () => {
+        const testResult = TestResult.failure('I failed');
+        expectFailureOn(testResult, 'I failed');
+    });
+
+    test('expectFailure fails if sent object is not a test result', () => {
+        assert.that(() => expectFailureOn(3, 'failureMessage')).raises(new Error(I18nMessage.of('invalid_object_in_test_result_assertion', typeof 3)))
+    });
+})
--- tests/support/assertion_helpers.js
+++ tests/support/assertion_helpers.js
@@ -1,20 +1,23 @@
-import { assert } from '../../lib/testy.js';
+import {assert, fail} from '../../lib/testy.js';
 import { I18nMessage } from '../../lib/i18n/i18n_messages.js';
 import { I18n } from '../../lib/i18n/i18n.js';
-
+import {TestResult} from "../../lib/core/test_result.js";
 const sourceCodeLocationRegex = /.* at .*:\d+:\d+/;
 
 const expectSuccess = result => {
   // my status
-  assert.isSuccessfulTestResult(result);
+  isSuccessfulTestResult(result);
   assert.isEmpty(result.location());
-
-  // other statuses
-  assert.isFalse(result.isFailure());
-  assert.isFalse(result.isError());
-  assert.isFalse(result.isPending());
-  assert.isFalse(result.isSkipped());
 };
+
+const isSuccessfulTestResult = (testResult) => {
+  validateIsTestResult(testResult)
+  const isSuccessfulTestResult = testResult.isSuccess();
+  if(!isSuccessfulTestResult) {
+    fail.reportFailure(testResult.hasFailureMessage() ? testResult.failureMessage() : I18nMessage.of('expected_test_result_fail', testResult.resultAsText()))
+  }
+  return testResult
+}
 
 const expectPendingResultOn = (result, reason) => {
   // my status
@@ -30,17 +33,27 @@ const expectPendingResultOn = (result, reason) => {
   assert.isFalse(result.isError());
 };
 
-const expectFailureOn = (result, failureMessage) => {
-  // my status
-  assert.isFailedTestResultWithMessage(result, failureMessage);
-  assert.areEqual(result.failureMessage(), failureMessage);
-  assert.that(result.location()).matches(sourceCodeLocationRegex);
+const validateIsTestResult = (object) => {
+  if (!(object instanceof TestResult)) throw new Error(I18nMessage.of('invalid_object_in_test_result_assertion', typeof object))
+}
+const validateIsExpectedFailureMessage = (actualFailureMessage, expectedFailureMessage) =>  {
+  const actualMessage = actualFailureMessage instanceof I18nMessage ? actualFailureMessage.expressedIn(I18n.default()) : actualFailureMessage;
+  const expectedMessage =  expectedFailureMessage instanceof I18nMessage ? expectedFailureMessage.expressedIn(I18n.default()) : expectedFailureMessage
+  const isSameFailureMessage = actualMessage === expectedMessage
+  if(!isSameFailureMessage) {
+    fail.reportFailure(I18nMessage.of('expectation_wrong_failure_message', expectedMessage, actualMessage))
+  }
+}
 
-  // other statuses
-  assert.isFalse(result.isSuccess());
-  assert.isFalse(result.isError());
-  assert.isFalse(result.isSkipped());
-  assert.isFalse(result.isPending());
+const expectFailureOn = (result, failureMessage) => {
+  validateIsTestResult(result)
+  const isFailedTestResult = result.isFailure()
+  if(!isFailedTestResult) {
+    fail.reportFailure(I18nMessage.of('expectation_failure_to_happen'))
+  }
+  validateIsExpectedFailureMessage(result.failureMessage(), failureMessage)
+  assert.that(result.location()).matches(sourceCodeLocationRegex);
+  return result
 };
 
 const expectErrorOn = (result, errorMessage, locationRegex = sourceCodeLocationRegex) => {
