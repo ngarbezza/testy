c73ac84e10c96d7748fd93bd3995424d104b0bcb
mariabelenamat <belen.amat29@gmail.com>
2025-04-19 13:02:55 -0300

    Adding tests for hasFailureMessage method

--- tests/core/assertions/test_result_assertions_test.js
+++ tests/core/assertions/test_result_assertions_test.js
@@ -22,4 +22,31 @@ suite('test result assertions', () => {
   test('expectFailure fails if sent object is not a test result', () => {
     assert.that(() => expectFailureOn(3, 'failureMessage')).raises(new Error(I18nMessage.of('invalid_object_in_test_result_assertion', typeof 3)));
   });
+
+  test('TestFailed result has failure message', () => {
+    const failedTestResult = TestResult.failure('I failed');
+    assert.that(failedTestResult.hasFailureMessage()).isTrue();
+    assert.that(failedTestResult.failureMessage()).isEqualTo('I failed');
+  });
+
+  test('TestErrored result has failure message', () => {
+    const erroredTestResult = TestResult.error('I got an error');
+    assert.that(erroredTestResult.hasFailureMessage()).isTrue();
+    assert.that(erroredTestResult.failureMessage()).isEqualTo('I got an error');
+  });
+
+  test('TestSucceeded result does not have failure message', () => {
+    const erroredTestResult = TestResult.success();
+    assert.that(erroredTestResult.hasFailureMessage()).isFalse();
+  });
+
+  test('TestExplicitlyMarkedPending result does not have failure message', () => {
+    const explicitlyMarkedPendingTestResult = TestResult.explicitlyMarkedAsPending('I am pending');
+    assert.that(explicitlyMarkedPendingTestResult.hasFailureMessage()).isFalse();
+  });
+
+  test('ExplicitlySkippedTest result does not have failure message', () => {
+    const explicitlySkippedTestResult = TestResult.explicitlySkip();
+    assert.that(explicitlySkippedTestResult.hasFailureMessage()).isFalse();
+  });
 });
